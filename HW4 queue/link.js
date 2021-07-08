1249bfd265e2b37fd6a9370afce4fdfca4ae6db9
artur.dzhkhinhan <1008480224@ukr.net>
2021-07-08 17:51:48 +0300

    tree

--- HW4 queue/link.js
+++ HW4 queue/link.js
@@ -58,60 +58,7 @@ class lList {
     this.list = newFirst
   }
 }
-
-class queue {
-  constructor(initValue = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {
-    this.mass = initValue
-  }
-  getList = () => {
-    return this.mass
-  }
-  lifo = () => {
-    return this.mass.pop()
-  }
-  fifo = () => {
-    return this.mass.shift()
-  }
-}
-
-class tree {
-  constructor(data = null) {
-    data ? this._addSt(data, this.root) : (this.root = null)
-  }
-
-  getList = () => {
-    return this.root
-  }
-
-  _addSt(data, point) {
-    point.data = data
-    point.left = null
-    point.right = null
-  }
-
-  add(data) {
-    if (this.root) {
-      let current = this.root
-      while (current) {
-        if (value === current.value) return undefined
-        if (value < current.value) {
-          if (current.left === null) {
-            current.left = newNode
-            return this
-          }
-          current = current.left
-        } else {
-          if (current.right === null) {
-            current.right = newNode
-            return this
-          }
-          current = current.right
-        }
-      }
-    }
-  }
-}
-
+console.log('list + fifo lifo')
 let oneDirectionList = new lList(1)
 oneDirectionList.addLast(2)
 oneDirectionList.addLast(3)
@@ -127,17 +74,108 @@ oneDirectionList.addLast('lifo')
 oneDirectionList.removeLast()
 console.log(oneDirectionList.getLastValue())
 console.log(oneDirectionList.getList())
+//------------------------------------------------------------------
+class queue {
+  constructor(initValue = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {
+    this.mass = initValue
+  }
+  getList = () => {
+    return this.mass
+  }
+  lifo = () => {
+    return this.mass.pop()
+  }
+  fifo = () => {
+    return this.mass.shift()
+  }
+}
 
-
-
+console.log('queue+stack')
 let qe = new queue()
 console.log(qe.fifo())
 console.log(qe.lifo())
 console.log(qe.getList())
+//------------------------------------------------------------------
+class tree {
+  constructor(data = null) {
+    this.root = this._addSt(data)
+  }
+
+  getList = () => {
+    return this.root
+  }
+
+  _addSt(data) {
+      return ( 
+    {value : data,
+    left : null,
+    right : null} )
+  }
+
+  add(data) {
+    if (this.root) {
+      let current = this.root
+      while (current) {
+        if (data === current.value) return undefined
+        if (data < current.value) {
+          if (current.left === null) {
+             current.left = this._addSt(data)
+            return this
+          }
+          current = current.left
+        } else {
+          if (current.right === null) {
+            current.right = this._addSt(data)
+            return this
+          }
+          current = current.right
+        }
+      }
+    }
+  }
+
+find(data){
+      if(!this.root) return false
+      
+      let current = this.root
+      let found = false
+      while(current && !found){
+            if(data < current.value){
+              current = current.left
+             } else if(data > current.value){
+                current = current.right
+             } else {
+                  found = current
+             } 
+            
+            }
+    
+        
+        return found
+      
+  
+  }
 
 
 
-let treee = new tree()
-console.log(qe.fifo())
-console.log(qe.lifo())
-console.log(qe.getList())
+}
+
+
+
+
+
+
+
+
+console.log('tree')
+const appleTree = new tree(1)
+appleTree.add(20)
+appleTree.add(30)
+appleTree.add(19)
+appleTree.add(18)
+
+
+console.log(appleTree.find(18))
+
+console.log(appleTree.getList())
+
